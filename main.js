83cdcc91e3363e962cc3862a69522b24790b06ec
Juanigna11 <juanisantos@live.com>
2024-05-29 11:01:42 -0300

    arreglos

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-// Cambio de cantidad de articulos ingresado por el usuario.
-
-const minusBtn = document.querySelector('.input__minus');
-const plusBtn = document.querySelector('.input__plus');
+let minusBtn = document.querySelector('.input__minus');
+let plusBtn = document.querySelector('.input__plus');
 let userInput = document.querySelector('.input__number');
 
 let userInputNumber = 0;
 
 plusBtn.addEventListener('click', ()=>{
     userInputNumber++;
-    userInput.value = userInputNumber;
     console.log(userInputNumber);
 
 });
@@ -18,7 +15,6 @@ minusBtn.addEventListener('click', ()=>{
     if(userInputNumber <= 0){
         userInputNumber = 0;
     }
-    userInput.value = userInputNumber;
     console.log(userInputNumber);
 });
 
